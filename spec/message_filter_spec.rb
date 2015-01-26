5c346a4ba70ea8008bbc198e89f3fac7b43b43a1
Kenta YAMAMOTO <lq33xxx@gmail.com>
2015-01-27 02:43:50 +0900

    Refactored

--- spec/message_filter_spec.rb
+++ spec/message_filter_spec.rb
@@ -2,4 +2,13 @@
 require_relative '../lib/message_filter'
 
 describe MessageFilter do
+  before(:each) do
+    @filter = MessageFilter.new('foo')
+  end
+  it 'detects message with NG word' do
+    expect(@filter.detect?('hello from foo')).to eq true
+  end
+  it 'does not detect message without NG word' do
+    expect(@filter.detect?('hello, world!')).to eq false
+  end
 end
