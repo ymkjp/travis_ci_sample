c441f7faca42c1d7f0d6cf96ca89e4dbb4c23b77
kenta.ky.yamamoto <kenta.ky.yamamoto@gree.net>
2015-01-27 09:54:44 +0900

    Implement variable length argument

--- lib/message_filter.rb
+++ lib/message_filter.rb
@@ -1,10 +1,13 @@
 
 class MessageFilter
-  def initialize(word)
-    @word = word
+  def initialize(*words)
+    @words = words
   end
 
   def detect?(text)
-    text.include?(@word)
+  	@words.each do |w|
+  		return true if text.include?(w)
+  	end
+  	false
   end
 end
