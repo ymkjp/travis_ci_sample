c61dae6b965e2ffec71b515c8c6ee8deb43e63b1
kenta.ky.yamamoto <kenta.ky.yamamoto@gree.net>
2015-01-27 09:42:40 +0900

    Refactored by subject

--- spec/message_filter_spec.rb
+++ spec/message_filter_spec.rb
@@ -1,14 +1,8 @@
 
 require_relative '../lib/message_filter'
 
-describe MessageFilter do
-  before do
-    @filter = MessageFilter.new('foo')
-  end
-  it 'detects message with NG word' do
-    expect(@filter).to be_detect('hello from foo')
-  end
-  it 'does not detect message without NG word' do
-    expect(@filter).not_to be_detect('hello, world!')
-  end
+describe MessageFilter, 'with argument "foo"' do
+  subject { MessageFilter.new('foo') }
+  it { is_expected.to be_detect('hello from foo') }
+  it { is_expected.not_to be_detect('hello, world!') }
 end
